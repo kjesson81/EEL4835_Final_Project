d5f5256a175ec178df35b63b6056a746d04f9f5d
kjesson81 <90724540+kjesson81@users.noreply.github.com>
2022-12-05 16:43:05 -0500

    Update synonym_with_DB.py

--- synonym_with_DB.py
+++ synonym_with_DB.py
@@ -10,6 +10,7 @@ from os.path import exists
 
 synonyms_dict = {}
 
+
 def get_synonyms_from_web(word):
     synonyms = []
     html_text = requests.get(f'https://www.thesaurus.com/browse/{word}').text
@@ -22,7 +23,6 @@ def get_synonyms_from_web(word):
         if link:
             text = link.text.strip()
             synonyms.append(text)
-            print(text)
 
     if not synonyms:
         synonyms = [word + " was not found. Either it is not in thesaurus.com, or the word does not exist"]
@@ -43,14 +43,13 @@ def synonyms(word):
         if user_input in synonyms_dict.keys():
             existing_words = []
             print(user_input + " has already been entered. The synonyms are:")
-            for existing_word in synonyms_dict.setdefault(user_input):
-                print(existing_word)
-                existing_words.append(existing_word)
-            synonyms_dict.setdefault(user_input, existing_words)
-            print('\n')
+            for value in synonyms_dict.setdefault(user_input):
+                print(value)
+
         else:
             synonyms_dict.setdefault(user_input, get_synonyms_from_web(user_input))
-
+            for value in synonyms_dict.setdefault(user_input):
+                print(value)
             # writes data to the JSON file
             with open("synonymsDB.json", "w") as file:
                 json.dump(synonyms_dict, file, indent=4)
@@ -63,7 +62,8 @@ def synonyms(word):
     return synonyms_dict
 
 
-run_code = input("Enter '1' if you want to type in your own words, and enter '2' if you want to read words from a file.\n")
+run_code = input(
+    "Enter '1' if you want to type in your own words, and enter '2' if you want to read words from a file.\n")
 
 if run_code == '1':
     user_input = input("Please enter a word to find it's synonyms!\n")
@@ -79,8 +79,41 @@ elif run_code == '2':
         for word in input_words:
             write_data.append(synonyms(word))
         length = len(write_data)
-        json.dump(write_data[length-1], output_file, indent=4)
+        json.dump(write_data[length - 1], output_file, indent=4)
         output_file.close()
         print("Synonyms have been written to a file called 'output.json'")
 else:
     print("You entered an invalid input. Goodbye.")
+
+with open("synonymsDB.json", "r") as data:
+    synonyms_dict = json.load(data)
+
+if __name__ == '__main__':
+    i = 0
+    x = True
+    while x:
+        with open("synonymsDB.json", "r") as data:
+            synonyms_dict = json.load(data)
+        time_wait = 3
+        time.sleep(time_wait)
+        i += 1
+
+        for existing_word in synonyms_dict.keys():
+            existing_synonyms = synonyms_dict[existing_word]
+            update = get_synonyms_from_web(existing_word)
+            print("Checking for update...")
+            if len(update) != len(existing_synonyms):
+                update_flag = True
+                synonyms_dict.setdefault(existing_word, update)
+                with open("synonymsDB.json", "w") as file:
+                    json.dump(synonyms_dict, file, indent=4)
+            else:
+                update_flag = False
+
+            if update_flag:
+                print("Updated synonyms for " + existing_word)
+            else:
+                print("no update needed for " + existing_word)
+
+        if i == 5:
+            x = False
