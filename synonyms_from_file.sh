6cc386caab420d2955807ea62a03c16020f3c353
kjesson81 <90724540+kjesson81@users.noreply.github.com>
2022-12-06 14:23:34 -0500

    Update synonyms_from_file.sh

--- synonyms_from_file.sh
+++ synonyms_from_file.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
-echo "Please enter the name of the file you wish to find the synonyms for:"
+echo "Please enter the name of the file you wish to find the synonyms for. If you want to enter a single word, press enter and wait for next prompt.:"
 read filename
 
 python3 synonyms.py $filename
